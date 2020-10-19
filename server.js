f4a966bbf169cdd716705567a3910e3bb1f729b3
Roshan Gupta <32669870+rgroshanrg@users.noreply.github.com>
2020-10-19 22:29:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ server.post('/', (req, res) => {
 server.get('*', (req, res) => {
     res.send("404");
 })
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+server.listen(5000, () => {
+    console.log("Server is running on port 5000");
 });
