38f86e18df722b41737d79c201c6f3138656b305
pooh54 <poojashreechoudhury7@gmail.com>
2021-10-16 11:19:56 +0530

    Update geocode.js

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,12 +1,13 @@
 const postmanRequest = require('postman-request');
 
 const geoCode = (address, callback) => {
-    const apiUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoicmdyb3NoYW5yZyIsImEiOiJja2VsZ3gwZDQwZ3dzMnptcXJvNnI2eXhoIn0.0WnakzXXvlk2IYHHq0xMLw&limit=1';
+    const accessToken = "pk.eyJ1Ijoicmdyb3NoYW5yZyIsImEiOiJja2VsZ3gwZDQwZ3dzMnptcXJvNnI2eXhoIn0.0WnakzXXvlk2IYHHq0xMLw"
+    const apiUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + accessToken +   '&limit=1';
     postmanRequest({url: apiUrl, json: true}, (error, res) => {
         if(error) {
-            callback('Internet Connection not Available', undefined);
+            callback('Please conect to the Internet', undefined);
         } else if(res.body.features.length === 0) {
-            callback('Unable to find the Address. Try a Valid Address.', undefined);
+            callback('Please try a Valid Address.', undefined);
         } else {
             callback(undefined, {latitude : res.body.features[0].center[1], longitude : res.body.features[0].center[0], location : res.body.features[0].place_name});
         }
